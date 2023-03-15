52f1d00795a14d473b8b38af3eb2348abb00df35
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2023-03-15 13:13:16 +0530

    create taskrowview , taksview function - task add display data sequence and add edit button and delete button for edit view then deleted task and used delete perform and edit fetch add data, then customsegmentview for today, upcoming task, Donetsk, failed task and add button add button for set background images

--- TaskUp/View/MainView.swift
+++ TaskUp/View/MainView.swift
@@ -8,8 +8,233 @@
 import SwiftUI
 
 struct MainView: View {
+    @StateObject var taskModel: TaskViewModel = .init()
+    //MARK: Matched Geometry Names
+    @Namespace var animation
+    
+//    MARK: Fetching Task
+    @FetchRequest(entity: Task.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Task.deadline, ascending: false)], predicate: nil, animation: .easeInOut) var tasks: FetchedResults<Task>
+    
+//    MARK: Environment values
+    @Environment(\.self) var env
+    @State var isOrdinary: Bool = true
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack{
+                VStack(alignment: .leading, spacing: 8){
+                    Text("Welcome Back")
+                        .font(.callout)
+                    Text("Here's Update Today")
+                        .font(.title2.bold())
+                }
+                
+                .frame(maxWidth: .infinity,  alignment: .leading)
+                //.padding(.vertical)
+                
+                
+                CustomSegmentedBar()
+            }
+            .padding()
+           // .padding(.top,5)
+ScrollView(.vertical, showsIndicators: false){
+        //MARK: Task View
+            TaskView()
+        .padding()
+        //Later will come
+        
+    }
+.padding(.vertical,-10)
+
+   }
+    .overlay(alignment: .bottom){
+
+        //MARK:Add button
+        Button{
+            taskModel.openEditTask.toggle()
+            
+        }label: {
+            Label{
+                Text("Add Task")
+                    .font(.callout)
+                    .fontWeight(.semibold)
+            }icon: {
+                Image(systemName: "plus.app.fill")
+            }
+            .foregroundColor(.white)
+            .padding(.vertical, 12)
+            .padding(.horizontal)
+            .background(.black, in: Capsule())
+            
+        }
+        //MARK:Linear GradientBG
+        .padding(.top, 10)
+        .frame(maxWidth: .infinity)
+        .background{
+            LinearGradient(colors: [
+                .white.opacity(0.05),
+                .white.opacity(0.4),
+                .white.opacity(0.7),
+                .white
+            ], startPoint: .top, endPoint: .bottom)
+            .ignoresSafeArea()
+        }
+    }
+    .fullScreenCover(isPresented: $taskModel.openEditTask){
+        taskModel.resetTaskData()
+    } content: {
+        AddNewTask()
+            .environmentObject(taskModel)
+    }
+}
+//    MARK: TaskView
+@ViewBuilder
+func TaskView()->some View{
+    LazyVStack(spacing: 20){
+       //MARK:
+        DynamicFilteredView(currentTab: taskModel.currentTab){ (task: Task) in
+            TaskRowView(task: task)
+        }
+    }
+    .padding(.top,20)
+}
+//    MARK: Task RowView
+func TaskRowView(task: Task)->some View{
+    VStack(alignment: .leading, spacing: 10){
+        HStack{
+            Text(task.type ?? "")
+                .font(.callout)
+                .padding(.vertical,5)
+                .padding(.horizontal)
+           
+                .background{
+                    if task.type == "Basic"{
+                    Capsule()
+                        .fill(Color("Green").opacity(0.7))
+                    }else if task.type == "Important"{
+                        Capsule()
+                            .fill(Color("Yellow").opacity(0.7))
+                    }else{
+                        Capsule()
+                            .fill(Color("Red").opacity(0.7))
+                    }
+            }
+            Spacer()
+            
+//                MARK:  Edit button only for noncomleted task
+            //&& taskModel.currentTab != "Failed Task"
+            if !task.isCompleted {
+                Button{
+                    taskModel.editTask = task
+                    taskModel.openEditTask = true
+                    taskModel.setupTask()
+                }label: {
+                    Image(systemName: "square.and.pencil")
+                        .foregroundColor(.black)
+                }
+            }
+            
+        }
+        .padding(.vertical,-10)
+        Text(task.title ?? "")
+            .font(.title3.bold())
+            .foregroundColor(.black)
+            .padding(.vertical,5)
+        
+        HStack(alignment: .bottom, spacing: 0){
+            VStack(alignment: .leading, spacing: 10){
+                HStack{
+                    Label {
+                        Text((task.deadline ?? Date()).formatted(date: .long, time: .omitted))
+                    } icon: {
+                        Image(systemName: "calendar")
+                    }
+                    .font(.caption)
+                    
+                    Label {
+                        Text((task.deadline ?? Date()).formatted(date: .omitted, time: .shortened))
+                    } icon: {
+                        Image(systemName: "clock")
+                    }
+                    .font(.caption)
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+          //  .padding(.vertical,-10)
+            //&& taskModel.currentTab != "Failed Task"
+            if !task.isCompleted {
+                Button{
+//                        MARK: updating Coredata
+                    task.isCompleted = true
+                  
+                    try? env.managedObjectContext.save()
+                } label: {
+                    Circle()
+                        .strokeBorder(.black,lineWidth: 1.5)
+                        .frame(width: 20, height: 20)
+                        .contentShape(Circle())
+                }
+            }
+            if task.isCompleted {
+                Button{
+//                        MARK: updating Coredata
+                    task.isCompleted = false
+                  
+                    try? env.managedObjectContext.save()
+                } label: {
+                    Circle()
+                        .strokeBorder(.black,lineWidth: 1.5)
+                        .frame(width: 20, height: 20)
+                        .contentShape(Circle())
+                }
+     
+                
+                
+            }
+               
+        }
+        .padding(.vertical,-10)
+        
+    }
+    .padding()
+    .frame(maxWidth: .infinity)
+    .background{
+        RoundedRectangle(cornerRadius: 12, style: .continuous)
+            .fill(Color(task.color ?? "Yellow").opacity(0.3))
+    }
+}
+//MARK: Custom Segmented Bar
+@ViewBuilder
+func CustomSegmentedBar()->some View{
+    let tabs = ["Today", "Upcoming", "Task Done", "Failed Task"]// In case missed card
+    VStack(alignment: .leading){
+        HStack(spacing: 20){
+            ForEach(tabs,id: \.self){tab in
+                Text(tab)
+                    .font(.callout)
+                    .fontWeight(.semibold)
+                    .scaleEffect(0.9)
+                    .foregroundColor(taskModel.currentTab == tab ? .white: .black)
+                // .padding(.horizontal,1)
+                    .padding(.vertical,6)
+                    .background{
+                        if taskModel.currentTab == tab{
+                            Capsule()
+                                .fill(.black)
+                            
+                                .matchedGeometryEffect(id: "TAB", in: animation)
+                            
+                            //.frame(width: 90, height: 25)
+                            // .padding()
+                        }
+                    }
+                    .contentShape(Capsule())
+                
+                    .onTapGesture {
+                        withAnimation{taskModel.currentTab = tab}
+                    }
+                }
+            }
+        }
     }
 }
 
